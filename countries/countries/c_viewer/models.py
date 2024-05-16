f528fbf840e4c367f0bc97ff0fc75610b08f864d
LichRave <artem.monkiewicz@gmail.com>
2024-05-16 20:29:05 +0200

    training from scratch, day 47 (16.05.24)

---
+++ countries/countries/c_viewer/models.py
@@ -0,0 +1,35 @@
+from django.db.models import Model, CharField, IntegerField, DecimalField
+
+# Create your models here.
+
+class Country(Model):
+    NA = 'NORTHERN AMERICA'
+    OC = 'OCEANIA'
+    EE = 'EASTERN EUROPE'
+    WE = 'WESTERN EUROPE'
+    LA = 'LATIN AMER. & CARIB'
+    BA = 'BALTICS'
+    AS = 'ASIA (EX. NEAR EAST)'
+    SA = 'SUB-SAHARAN AFRICA'
+    NE = 'NEAR EAST'
+    ID = 'C.W. OF IND. STATES'
+    NF = 'NORTHERN AFRICA'
+    region_choices = {
+        (NA, 'NORTHERN AMERICA'),
+        (OC, 'OCEANIA'),
+        (EE, 'EASTERN EUROPE'),
+        (WE, 'WESTERN EUROPE'),
+        (LA, 'LATIN AMER. & CARIB'),
+        (BA, 'BALTICS'),
+        (AS, 'ASIA (EX. NEAR EAST)'),
+        (SA, 'SUB-SAHARAN AFRICA'),
+        (NE, 'NEAR EAST'),
+        (ID, 'C.W. OF IND. STATES'),
+        (NF, 'NORTHERN AFRICA')
+    }
+    name = CharField(max_length=500)
+    region = CharField(max_length=500, choices=region_choices)
+    population = IntegerField()
+    population_density = DecimalField(decimal_places=2, max_digits=20)
+    gdp = DecimalField(decimal_places=2, max_digits=20)
+    gdp_per_capita = DecimalField(decimal_places=2, max_digits=20)
